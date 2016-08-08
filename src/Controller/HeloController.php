fb3d1247d77b96674016ce11a727345c64a044fc
vagrant <vagrant@debian>
2016-08-08 12:13:02 +0900

    a

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -5,26 +5,8 @@ use App\Controller\AppController;
 
 class HeloController extends AppController
 {
-	public function index($a = '', $b = '')
-	{
-		if ($a == '') {
-			$this->setAction('err');
-			return;
-		}
-		$this->autoRender = false;
-		echo "<html><head></head><body>";
-		echo "<h1>Hello!</h1>";
-		echo "<p>これは、サンプルで作成したページです。</p><p>";
-		if ($a != '') {
-			echo " パラメータA: " . $a;
-		}
-		if ($b != '') {
-			echo " パラメータB: " . $b;
-		}
-		echo "</p></body></html>";
-	}
-
-	public function err()
+	public function index()
+#	public function err()
 	{
 		$this->autoRender = false;
 		echo "<html><head></head><body>";
