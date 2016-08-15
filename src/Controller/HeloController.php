326014a26c23b41b0ca7d7c872ca78aae88a1f0f
vagrant <vagrant@debian>
2016-08-15 10:01:53 +0900

    8-15

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -5,13 +5,23 @@ use App\Controller\AppController;
 
 class HeloController extends AppController
 {
-	public function index()
-#	public function err()
-	{
-		$this->autoRender = false;
-		echo "<html><head></head><body>";
-		echo "<h1>Hello!</h1>";
-		echo "<p>パラメータがありませんでした。</p>";
-		echo "</body></html>";
-	}
+		public function index($a = '',$b = '')
+				{
+					$this->set('message', 'Hello! this is sample page. ;-)');	
+					$str = $this->request->data('text1');
+					if ($str != null){
+							$this->set('message', 'you typed:' . $str);
+					} else {
+							$this->set('message','please type...');
+					}
+				}
+			  public function err()
+				{
+						$this->autoRender = false;
+						echo "<html><head></head><body>";
+						echo "<h1>Hello!</h1>";
+						echo "<p>パラメータがありませんでした。</p>";
+						echo "</body></html>";
+				}
 }
+
--- src/Template/Helo/index.ctp
+++ src/Template/Helo/index.ctp
@@ -1,6 +1,15 @@
 <div>
-    <h3>Index Page</h3>
-    <ul class="side-nav">
-        <li>This is sample.</li>
-    </ul>
+
+	<h3>Index Page</h3>
+
+	<p><?= $message ?></p>
+
+	<form method="post" action="samplecake/helo/index">
+
+	<input type="text" name="text1">
+
+	<input type="submit">
+
+	</form>
+
 </div>
